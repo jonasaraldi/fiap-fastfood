53e058afcc32a88679d7572cb07f389ba6489c17
jonas.araldi <jonas.araldi@amcom.com.br>
2023-10-02 18:45:02 -0300

    feat: estrutura inicial do monolito modular usando Carter

---
+++ src/FastFood.WebApi/Program.cs
@@ -0,0 +1,19 @@
+using Carter;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapCarter();
+app.Run();
---
+++ src/Modules/Atendimento/src/FastFood.Atendimento.Endpoints/AtendimentoModule.cs
@@ -0,0 +1,29 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace FastFood.Pedido.Endpoints;
+
+public class PedidoModule : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        var pedido = app.MapGroup("pedidos");
+        
+        pedido.MapGet("", () => 
+            Results.Ok("Lista de pedidos"));
+        
+        pedido.MapPost("", () => 
+            Results.Ok("Pedido salvo"));
+        
+        pedido.MapPost("itens", () => 
+            Results.Ok("Novo item adicionado"));
+        
+        pedido.MapPatch("confirmado", () => 
+            Results.Ok("Situacao do pedido alterada para confirmada"));
+        
+        pedido.MapPatch("finalizado", () => 
+            Results.Ok("Situacao do pedido alterada para finalizada"));
+    }
+}
