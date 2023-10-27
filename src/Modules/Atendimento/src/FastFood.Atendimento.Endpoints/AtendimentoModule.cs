d329a56cf91e2fe25e9d815ca047b4c3f39afdad
jonas.araldi <jonas.araldi@amcom.com.br>
2023-10-27 19:40:05 -0300

    chore: melhorando documentação do swagger

--- src/Modules/Atendimento/src/FastFood.Atendimento.Endpoints/AtendimentoModule.cs
+++ src/Modules/Atendimento/src/FastFood.Atendimento.Endpoints/AtendimentoModule.cs
@@ -4,7 +4,6 @@ using FastFood.Atendimento.Application.Services.Pedidos.Commands.AtualizarCpf;
 using FastFood.Atendimento.Application.Services.Pedidos.Commands.CancelarPedido;
 using FastFood.Atendimento.Application.Services.Pedidos.Commands.ConfirmarPedido;
 using FastFood.Atendimento.Application.Services.Pedidos.Commands.CriarPedido;
-using FastFood.Atendimento.Application.Services.Pedidos.Commands.FinalizarPedido;
 using FastFood.Atendimento.Application.Services.Pedidos.Commands.IdentificarCliente;
 using FastFood.Atendimento.Application.Services.Pedidos.Commands.RemoverItemDePedido;
 using FastFood.Atendimento.Application.Services.Pedidos.Queries.GetConsultaDePedidos;
@@ -22,111 +21,147 @@ public class AtendimentoModule : ICarterModule
 {   
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        var pedido = app.MapGroup("pedidos");
-        
+        var pedido = app.MapGroup("pedidos")
+            .WithTags("Pedido");
+
         pedido.MapGet("", async (
-            [FromQuery]DateTime? dataInicial,
-            [FromQuery]DateTime? dataFinal,
-            ISender sender,
-            CancellationToken cancellationToken) =>
-        {
-            var query = new GetConsultaDePedidosQuery(
-                dataInicial, dataFinal);
-            
-            var response = await sender.Send(query, cancellationToken);
-            return Results.Ok(response);
-        });
-        
+                [FromQuery] DateTime? dataInicial,
+                [FromQuery] DateTime? dataFinal,
+                ISender sender,
+                CancellationToken cancellationToken) =>
+            {
+                var query = new GetConsultaDePedidosQuery(
+                    dataInicial, dataFinal);
+
+                var response = await sender.Send(query, cancellationToken);
+                return TypedResults.Ok(response);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Consulta de pedidos",
+                Description = "Retorna uma lista de pedidos dentro de um período estipulado."
+            });
+
         pedido.MapGet("confirmados", async (
-            ISender sender,
-            CancellationToken cancellationToken) =>
-        {
-            var response = await sender.Send(new GetPedidosConfirmadosQuery(), cancellationToken);
-            return Results.Ok(response);
-        });
-        
+                ISender sender,
+                CancellationToken cancellationToken) =>
+            {
+                var response = await sender.Send(new GetPedidosConfirmadosQuery(), cancellationToken);
+                return TypedResults.Ok(response);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Consulta a fila de pedidos da cozinha",
+                Description = "Retorna todos os pedidos confirmados do dia atual, essa é a fila de pedidos enviada para a cozinha preparar."
+            });
+            
         pedido.MapPut("{pedidoId}/cliente", async (
-            Ulid pedidoId,
-            [FromBody]IdentificarClienteRequest request,
-            ISender sender,
-            CancellationToken cancellationToken) =>
-        {
-            IdentificarClienteCommand command = new(pedidoId, request.Nome, request.Email); 
-            await sender.Send(command, cancellationToken);
-            return Results.NoContent();
-        });
+                Ulid pedidoId,
+                [FromBody]IdentificarClienteRequest request,
+                ISender sender,
+                CancellationToken cancellationToken) =>
+            {
+                IdentificarClienteCommand command = new(pedidoId, request.Nome, request.Email); 
+                await sender.Send(command, cancellationToken);
+                return TypedResults.NoContent();
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Registra cliente (nome e e-mail)",
+                Description = "Registra o cliente na base com nome e e-mail."
+            });
         
         pedido.MapPost("", async (
-            ISender sender,
-            CancellationToken cancellationToken) =>
-        {
-            var response = await sender.Send(new CriarPedidoCommand(), cancellationToken);
-            return Results.Ok(response);
-        });
-        
+                ISender sender,
+                CancellationToken cancellationToken) =>
+            {
+                var response = await sender.Send(new CriarPedidoCommand(), cancellationToken);
+                return TypedResults.Ok(response);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Cria um novo pedido",
+                Description = "Cria um novo pedido do zero, sem itens e cliente. Essa é a chamada que da início a todo o processo do pedido."
+            });
+
         pedido.MapPut("{pedidoId}/cpf", async (
-            Ulid pedidoId,
-            [FromBody]AtualizarCpfRequest request,
-            ISender sender, 
-            CancellationToken cancellationToken) =>
-        {
-            AtualizarCpfCommand command = new(pedidoId, request.cpf);
-            await sender.Send(command, cancellationToken);
-            return Results.NoContent();
-        });
+                Ulid pedidoId,
+                [FromBody]AtualizarCpfRequest request,
+                ISender sender, 
+                CancellationToken cancellationToken) =>
+            {
+                AtualizarCpfCommand command = new(pedidoId, request.cpf);
+                await sender.Send(command, cancellationToken);
+                return TypedResults.NoContent();
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Registra o CPF do cliente",
+                Description = "Registra o CPF do cliente para posteriormente gera a nota fiscal com o CPF."
+            });
         
         pedido.MapPost("{pedidoId}/itens", async (
-            Ulid pedidoId, 
-            [FromBody]AdicionarItemDePedidoRequest request, 
-            ISender sender, 
-            CancellationToken cancellationToken) =>
-        {
-            AdicionarItemDePedidoCommand command = new(
-                pedidoId, request.Nome, request.Descricao, request.Preco, request.Quantidade, request.Observacao);
-            
-            var response = await sender.Send(command, cancellationToken);
-            return Results.Ok(response);
-        });
+                Ulid pedidoId, 
+                [FromBody]AdicionarItemDePedidoRequest request, 
+                ISender sender, 
+                CancellationToken cancellationToken) =>
+            {
+                AdicionarItemDePedidoCommand command = new(
+                    pedidoId, request.Nome, request.Descricao, request.Preco, request.Quantidade, request.Observacao);
+                
+                var response = await sender.Send(command, cancellationToken);
+                return TypedResults.Ok(response);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Adiciona item ao pedido",
+                Description = "Adiciona um novo item ao pedido informado."
+            });
         
         pedido.MapDelete("{pedidoId}/itens/{id}", async (
-            Ulid pedidoId, 
-            Ulid id, 
-            ISender sender, 
-            CancellationToken cancellationToken) =>
-        {
-            RemoverItemDePedidoCommand command = new(pedidoId, id);
-            var response = await sender.Send(command, cancellationToken);
-            return Results.Ok(response);
-        });
+                Ulid pedidoId, 
+                Ulid id, 
+                ISender sender, 
+                CancellationToken cancellationToken) =>
+            {
+                RemoverItemDePedidoCommand command = new(pedidoId, id);
+                var response = await sender.Send(command, cancellationToken);
+                return TypedResults.Ok(response);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Remove item do pedido",
+                Description = "Remove um item específico do pedido informado."
+            });
         
         pedido.MapPut("{pedidoId}/confirmar", async (
-            Ulid pedidoId,
-            ISender sender, 
-            CancellationToken cancellationToken) =>
-        {
-            ConfirmarPedidoCommand command = new(pedidoId);
-            var response = await sender.Send(command, cancellationToken);
-            return Results.Ok(response);
-        });
+                Ulid pedidoId,
+                ISender sender, 
+                CancellationToken cancellationToken) =>
+            {
+                ConfirmarPedidoCommand command = new(pedidoId);
+                var response = await sender.Send(command, cancellationToken);
+                return TypedResults.Ok(response);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Confirma o pedido",
+                Description = "Confirma o pedido indicando que ele está pronto para ser feito pela cozinha (após pago)."
+            });
         
         pedido.MapPut("{pedidoId}/cancelar", async (
-            Ulid pedidoId,
-            ISender sender, 
-            CancellationToken cancellationToken) =>
-        {
-            CancelarPedidoCommand command = new(pedidoId);
-            var response = await sender.Send(command, cancellationToken);
-            return Results.Ok(response);
-        });
-        
-        // pedido.MapPut("{pedidoId}/finalizar", async (
-        //     Ulid pedidoId,
-        //     ISender sender, 
-        //     CancellationToken cancellationToken) =>
-        // {
-        //     var command = new FinalizarPedidoCommand(pedidoId);
-        //     var response = await sender.Send(command, cancellationToken);
-        //     return Results.Ok(response);
-        // });
+                Ulid pedidoId,
+                ISender sender, 
+                CancellationToken cancellationToken) =>
+            {
+                CancelarPedidoCommand command = new(pedidoId);
+                var response = await sender.Send(command, cancellationToken);
+                return TypedResults.Ok(response);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Cancela o pedido",
+                Description = "Cancela o pedido informado caso já não esteja confirmado."
+            });
     }
 }
--- src/Modules/Catalogo/src/FastFood.Catalogo.Endpoints/CatalogoModule.cs
+++ src/Modules/Catalogo/src/FastFood.Catalogo.Endpoints/CatalogoModule.cs
@@ -18,74 +18,101 @@ public class CatalogoModule : ICarterModule
 {   
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        var categoria = app.MapGroup("categorias");
-        
+        var categoria = app.MapGroup("categorias")
+            .WithTags("Catalogo");
+
         categoria.MapGet("", async (
-            ISender sender,
-            CancellationToken cancellationToken) =>
-        {
-            var response = await sender.Send(
-                new GetCategoriasQuery(), cancellationToken);
-            
-            return Results.Ok(response);
-        });
+                ISender sender,
+                CancellationToken cancellationToken) =>
+            {
+                var response = await sender.Send(
+                    new GetCategoriasQuery(), cancellationToken);
+
+                return Results.Ok(response);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Consulta de categorias",
+                Description = "Retorna todas as categorias de produto disponíveis."
+            });
+
+        var produto = app.MapGroup("produtos")
+            .WithTags("Catalogo");
         
-        var produto = app.MapGroup("produtos");
-        
-        produto.MapGet("{categoria}", async (
-            string categoria,
-            ISender sender,
-            CancellationToken cancellationToken) =>
-        {
-            var response = await sender.Send(
-                new GetProdutosPorCategoriaQuery(categoria), cancellationToken);
-            
-            return Results.Ok(response);
-        });
+        produto.MapGet("categoria/{categoria}", async (
+                string categoria,
+                ISender sender,
+                CancellationToken cancellationToken) =>
+            {
+                var response = await sender.Send(
+                    new GetProdutosPorCategoriaQuery(categoria), cancellationToken);
+                
+                return Results.Ok(response);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Consulta de produtos por categoria",
+                Description = "Retorna todos os produtos da categoria informada."
+            });
         
         produto.MapPost("", async (
-            ISender sender,
-            [FromBody]ProdutoRequest request,
-            CancellationToken cancellationToken) =>
-        {
-            var command = new AdicionarProdutoCommand(
-                request.Nome, 
-                request.Descricao, 
-                request.Categoria, 
-                request.Preco, 
-                request.UrlDaImagem);
-            
-            var response = await sender.Send(command, cancellationToken);
-            return Results.Ok(response);
-        });
+                ISender sender,
+                [FromBody]ProdutoRequest request,
+                CancellationToken cancellationToken) =>
+            {
+                var command = new AdicionarProdutoCommand(
+                    request.Nome, 
+                    request.Descricao, 
+                    request.Categoria, 
+                    request.Preco, 
+                    request.UrlDaImagem);
+                
+                var response = await sender.Send(command, cancellationToken);
+                return Results.Ok(response);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Adiciona um novo produto",
+                Description = "Adiciona um novo produto ao catálogo."
+            });
         
         produto.MapPut("{id}", async (
-            Ulid id,
-            [FromBody]ProdutoRequest request,
-            ISender sender,
-            CancellationToken cancellationToken) =>
-        {
-            var command = new EditarProdutoCommand(
-                id,
-                request.Nome, 
-                request.Descricao, 
-                request.Categoria, 
-                request.Preco, 
-                request.UrlDaImagem);
-            
-            await sender.Send(command, cancellationToken);
-            return Results.NoContent();
-        });
+                Ulid id,
+                [FromBody]ProdutoRequest request,
+                ISender sender,
+                CancellationToken cancellationToken) =>
+            {
+                var command = new EditarProdutoCommand(
+                    id,
+                    request.Nome, 
+                    request.Descricao, 
+                    request.Categoria, 
+                    request.Preco, 
+                    request.UrlDaImagem);
+                
+                await sender.Send(command, cancellationToken);
+                return Results.NoContent();
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Edita um produto",
+                Description = "Edita um produto específico do catálogo."
+            });
         
         produto.MapDelete("{id}", async (
-            Ulid id,
-            ISender sender,
-            CancellationToken cancellationToken) =>
-        {
-            var command = new RemoverProdutoCommand(id);
-            
-            await sender.Send(command, cancellationToken);
-            return Results.NoContent();
-        });
+                Ulid id,
+                ISender sender,
+                CancellationToken cancellationToken) =>
+            {
+                var command = new RemoverProdutoCommand(id);
+                
+                await sender.Send(command, cancellationToken);
+                return Results.NoContent();
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Remove um produto",
+                Description = "Remove um produto específico do catálogo."
+            });
     }
 }
